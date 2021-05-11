79d7ff3f49a0081ba94bb50682687a4af90687ba
Drishti Maharjan <44743273+drishti-m@users.noreply.github.com>
2021-05-11 23:44:16 +0200

    Add documentation for fluid segmentation

--- thesis-code/plugin-src/fluid_segment_plugin.py
+++ thesis-code/plugin-src/fluid_segment_plugin.py
@@ -1,21 +1,32 @@
-# from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
+  
+"""
+This is the plugin code for fluid segmentation.
+Accepted input data models: VTK Rectilinear Grid
+Output data model: VTK Rectilinear Grid
+It takes from user two parameters: Trained Model's Path, Model's Class Defn Path.
+The path can be either absolute or relative to Paraview's binary executable location.
+This plugin is designed to segment a grid such that cells with magnitude greater than the threshold value are
+labelled as 1, otherwise labelled as 0.
+Threshold value is determined during training of model.
+"""
+
 from paraview.util.vtkAlgorithm import *
+from paraview.vtk.util import numpy_support as ns
+from paraview import simple
+import vtk
+from vtk.util import numpy_support
+from vtkmodules.numpy_interface import dataset_adapter as DA
 import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import os
-import vtk
-from vtk.util import numpy_support
-from paraview.vtk.util import numpy_support as ns
-from paraview import simple
 import numpy as np
-from vtkmodules.numpy_interface import dataset_adapter as DA
 
 
 @smproxy.filter()
 @smproperty.input(name="InputRectilinear", port_index=0)
-@smdomain.datatype(dataTypes=["vtkRectilinearGrid", "vtkImageData"], composite_data_supported=True)
+@smdomain.datatype(dataTypes=["vtkRectilinearGrid"], composite_data_supported=True)
 class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
     input_data_type = ""
     t_port = 0
@@ -26,7 +37,8 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
     def __init__(self):
         VTKPythonAlgorithmBase.__init__(
             self, nInputPorts=1, nOutputPorts=1, outputType="vtkRectilinearGrid")
-
+        
+    # First step in pipeline: set Port info
     def FillInputPortInformation(self, port, info):
         if port == 0:
             self.t_port = port
@@ -35,6 +47,7 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
         print("port info set")
         return 1
 
+     # Request Data from input and create output
     def RequestData(self, request, inInfoVec, outInfoVec):
         from vtkmodules.vtkCommonDataModel import vtkRectilinearGrid, vtkImageData
         from vtkmodules.vtkCommonCore import VTK_DOUBLE
@@ -42,47 +55,30 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
         self.input_data_type = self.GetInputDataObject(
             self.t_port, self.t_index).GetClassName()
         if self.input_data_type == "vtkRectilinearGrid":
-            x, y, z, xCoords, yCoords, zCoords, vtk_double_array = self.Process_RectlinearGrid(
+            x, y, z, xCoords, yCoords, zCoords, vtk_output_array = self.Process_RectlinearGrid(
                 inInfoVec, outInfoVec)
-        elif self.input_data_type == "vtkImageData":
-            x, y, z, xCoords, yCoords, zCoords, vtk_double_array = self.Convert_Img_To_Rectilinear(
-                inInfoVec)
+
         output = vtkRectilinearGrid.GetData(outInfoVec, 0)
         output.SetDimensions(x, y, z)
         output.SetXCoordinates(xCoords)
         output.SetYCoordinates(yCoords)
         output.SetZCoordinates(zCoords)
 
-        output.GetPointData().SetScalars(vtk_double_array)
+        output.GetPointData().SetScalars(vtk_output_array)
 
         return 1
 
-    def Convert_Img_To_Rectilinear(self, inInfoVec):
-        from vtkmodules.vtkCommonDataModel import vtkRectilinearGrid, vtkImageData
-        from vtkmodules.vtkCommonCore import VTK_DOUBLE
-        pdi = vtkImageData.GetData(inInfoVec[0], 0)
-        x, y, z = pdi.GetDimensions()
-        xCoords = np.arange(0, x, 1)
-        yCoords = np.arange(0, y, 1)
-        zCoords = np.arange(0, z, 1)
-        xCoords = DA.numpyTovtkDataArray(
-            xCoords, name="x-coordinates")
-        yCoords = DA.numpyTovtkDataArray(
-            yCoords, name="y-coordinates")
-        zCoords = DA.numpyTovtkDataArray(
-            zCoords, name="z-coordinates")
-
-        no_arrays = pdi.GetPointData().GetNumberOfArrays()
-        vtk_double_array = pdi.GetPointData().GetAbstractArray(0)
-        numpy_array = ns.vtk_to_numpy(vtk_double_array)
-
-        torch_np_array = self.trained_threshold(numpy_array)
-
-        vtk_double_array = DA.numpyTovtkDataArray(
-            torch_np_array, name="threshold_pixels")
-        return x, y, z, xCoords, yCoords, zCoords, vtk_double_array
-
     def Process_RectlinearGrid(self, inInfoVec, outInfoVec):
+        """
+        Helper function to process info from input to send it for model inference
+        Args:
+        inInfoVec: input information vector
+        outInfoVec: output information vector
+        Returns:
+        x, y, z: output x,y,z dimensions respectively
+        xCoords, yCoords, zCoords: output's x co-ordinates, y-coordinates, z-coordinates respectively
+        output_vtk_array: VTK array for output
+        """
         from vtkmodules.vtkCommonDataModel import vtkRectilinearGrid, vtkImageData
         from vtkmodules.vtkCommonCore import VTK_DOUBLE
         pdi = vtkRectilinearGrid.GetData(inInfoVec[0], 0)
@@ -94,28 +90,31 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
         no_arrays = pdi.GetPointData().GetNumberOfArrays()
         float_array = pdi.GetPointData().GetAbstractArray(0)
         no_components = float_array.GetNumberOfComponents()
-        numpy_array = ns.vtk_to_numpy(float_array)
+        input_numpy_array = ns.vtk_to_numpy(float_array)
 
-        torch_np_array = self.trained_threshold(numpy_array, no_components)
+        output_np_array = self.Segment_Grid(input_numpy_array, no_components)
 
-        vtk_double_array = DA.numpyTovtkDataArray(
-            torch_np_array, name="threshold_pixels")
-        # print(vtk_double_array)
-        # vtk_double_array.SetNumberOfComponents(
-        #     float_array.GetNumberOfComponents())
+        output_vtk_array = DA.numpyTovtkDataArray(
+            output_np_array, name="threshold_pixels")
 
-        return x, y, z, xCoords, yCoords, zCoords, vtk_double_array
+        return x, y, z, xCoords, yCoords, zCoords, output_vtk_array
 
-    def trained_threshold(self, numpy_array, no_components):
+    def Segment_Grid(self, numpy_array, no_components):
+        """
+        Performs segmentation of grid according to model loaded.
+        Args:
+        numpy_array: Numpy array of magnitude of all cells of grid
+        no_components: number of components of input VTK array
+        Returns:
+        segmented_np_array: numpy array with segmented labels for all cells
+        """
         from importlib import import_module
 
         module_name = self.get_trained_class_module_name()
-        #module_name = "neural_net"
         module = import_module(module_name)
 
         net = module.Net()
         net.load_state_dict(torch.load(self.model_path))
-        # numpy_array = numpy_array.reshape((1, -1))[0]
         print(numpy_array.shape)
 
         torch_array = torch.from_numpy(numpy_array.copy()).to(torch.float)
@@ -123,22 +122,14 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
             torch_array = torch.unsqueeze(torch_array, 1)
         print(torch_array.shape)
 
-        criterion = nn.CrossEntropyLoss()
-        model_outputs = net.forward(torch_array)
-        mag = np.array([np.sqrt(x.dot(x)) for x in torch_array])
-        exp_output = mag > 0.01000
-        exp_output = exp_output.astype(float)
-        exp_output = torch.from_numpy(exp_output).to(torch.long)
-        v_loss = criterion(model_outputs, exp_output)
-        timestep = self.GetInputDataObject(0, 0).GetInformation().Get(
-            vtk.vtkDataObject.DATA_TIME_STEP())
-        # print("Loss in timestep", timestep, " = ", v_loss.item())
-
         outputs = net.predict(torch_array)
-        # print(outputs)
-        return outputs.detach().numpy()
+        segmented_np_array = outputs.detach().numpy()
+        return segmented_np_array
 
     def get_trained_class_module_name(self):
+        """
+        Get module name of class definition(architecture) of the trained model
+        """
         import sys
         import os.path
         from pathlib import Path
@@ -150,13 +141,13 @@ class ML_Fluid_Segmentation(VTKPythonAlgorithmBase):
         module_name = Path(self.class_path).stem
         return module_name
 
-    @ smproperty.stringvector(name="Trained Model Path")
+    @ smproperty.stringvector(name="Trained Model's Path")
     def SetModelPathR(self, x):
         print("Model path: ", x)
         self.model_path = x
         self.Modified()
 
-    @ smproperty.stringvector(name="Model's Class Path")
+    @ smproperty.stringvector(name="Model's Class Defn Path")
     def SetClassPath(self, x):
         print("Class path: ", x)
         self.class_path = x
